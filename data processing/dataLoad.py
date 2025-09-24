4db49a0959e1e5fa762f50a601e84682b5b0b634
RyanFellDown <ryanfell10@gmail.com>
2025-09-24 14:45:45 -0700

    Preprocessing Complete, 2.3 Mil Rows added to Postgres DB

--- data processing/dataLoad.py
+++ data processing/dataLoad.py
@@ -11,7 +11,7 @@ cmsData = os.path.join(baseDirectory, 'data', 'raw', 'Medicare_Physician_Other_P
 hcpcsCodesData = os.path.join(baseDirectory, 'data', 'processed', 'HCPS Codes - Sheet1.csv')
 
 
-data = pd.read_csv(cmsData, usecols=["Rndrng_NPI", "Rndrng_Prvdr_Zip5", "HCPCS_Cd", "HCPCS_Desc", "Tot_Benes", "Tot_Srvcs", "Avg_Sbmtd_Chrg", "Avg_Mdcr_Alowd_Amt", "Avg_Mdcr_Pymt_Amt"], nrows=10000)
+data = pd.read_csv(cmsData, usecols=["Rndrng_NPI", "Rndrng_Prvdr_Zip5", "HCPCS_Cd", "HCPCS_Desc", "Tot_Benes", "Tot_Srvcs", "Avg_Sbmtd_Chrg", "Avg_Mdcr_Alowd_Amt", "Avg_Mdcr_Pymt_Amt"])
 hcpcsCodes = pd.read_csv(hcpcsCodesData)
 data["Estimated_Average_Payment"] = data["Avg_Mdcr_Alowd_Amt"] - data["Avg_Mdcr_Pymt_Amt"]
 print(data.shape)
@@ -25,27 +25,29 @@ for code in hcpcsCodes["HCPCS Code"]:
     codeIndex += 1
 
 
+
 #1. Remove ANY NaN rows from the entire dataframe (aka if anything is missing in a row, then it shouldn't be included).
 data = data.dropna()
+
 
 
 #2. Checking if any Zip codes are incorrect format (either too long, too short, 99999, or 00000)
 #   Check if the code in the data is one of the defined HCPCS Codes listed. If not, remove.
 #   Will continue to expand the number of codes available, otherwise this is a limiting factor.
-rowIndex = 0
-for _, row in data.iterrows():
-    zip = str(row["Rndrng_Prvdr_Zip5"])
-    code = row["HCPCS_Cd"]
 
-    #Run through all these parts; if the row is NOT dropped, then we can use it in our values for the averages.
-    if zip == ("99999" or "00000"):
-        data.drop(rowIndex)
-    elif len(zip) < 5 or len(zip) > 5:
-        data.drop(rowIndex)
-    elif code not in hcpcsHash:
-        data.drop(rowIndex)
+#   Convert all numpy.ints to strings
+data['Rndrng_Prvdr_Zip5'] = data['Rndrng_Prvdr_Zip5'].astype(str).str.strip()
+data = data.dropna(subset=['Rndrng_Prvdr_Zip5','HCPCS_Cd'])
 
-    rowIndex += 1
+#   Drop if the zip codes are 99999 or 00000, fake codes, and add leading zeroes if it's shorter than 5 digits.
+data = data[data["Rndrng_Prvdr_Zip5"] != "99999"]
+data = data[data["Rndrng_Prvdr_Zip5"] != "000000"]
+data['Rndrng_Prvdr_Zip5'] = data['Rndrng_Prvdr_Zip5'].str.zfill(5)
+
+#   Drop if it's NOT an HCPCS code, which are pulled from a separate CSV (hence why we need to filter as such).
+data = data[~data['HCPCS_Cd'].isin(hcpcsHash)]
+print(data.head)
+
 
 
 #3. Group all the charges by their zip codes and then return the average of each one, giving us the the average
@@ -54,6 +56,7 @@ for _, row in data.iterrows():
 
 #   Group by ZIP + HCPCS.
 grouped = data.groupby(['Rndrng_Prvdr_Zip5', 'HCPCS_Cd'])
+print("number of groups:", grouped.ngroups)
 
 #   Aggregate all the means of the payments per procedure per zip code.
 zip_stats_df = grouped.agg({
@@ -63,27 +66,35 @@ zip_stats_df = grouped.agg({
     'Estimated_Average_Payment': 'mean',
     'Rndrng_NPI': pd.Series.nunique
 }).reset_index()
+
+print("ZIP_DF Check 1: ", zip_stats_df)
+
 zip_stats_df.rename(columns={'Rndrng_NPI': 'provider_count'}, inplace=True)
 
 #   Checking if there are in fact multiple providers who offer a procedure per zip code.
-print(zip_stats_df[zip_stats_df['provider_count'] >= 3])
+#   Returns an Empty DF if none of the provider counts are greater than three.
+print("ZIP_DF Check 2: ", zip_stats_df[zip_stats_df['provider_count'] >= 3])
 
+
+password = input("Enter you password here.")
 
 #   Now, we've grouped zip codes with respective HCPCS codes. We can add this to the PostgreSQL database.
-conn_string = 'postgresql://postgres:hidden@localhost:5432/postgres'
+conn_string = f'postgresql://postgres:{password}@localhost:5432/postgres'
 
 db = create_engine(conn_string)
 conn = db.connect()
+print("Connection 1 worked.")
 
 zip_stats_df.to_sql('zip_stats_df', con=conn, if_exists='replace', index=False)
 conn = psycopg2.connect(conn_string)
 conn.autocommit = True
 cursor = conn.cursor()
+print("Connection 2 and data to DF woked.")
 
 sql1 = '''select * from zip_stats_df;'''
 cursor.execute(sql1)
 for i in cursor.fetchall():
     print(i)
 
-# conn.commit()
+conn.commit()
 conn.close()
